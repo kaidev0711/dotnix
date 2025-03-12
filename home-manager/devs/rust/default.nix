d7760f28fcc2842cfa143650bcce79602e2e8bef
Dang Van Tuan <kaiz.developers@gmail.com>
2025-03-13 00:54:26 +0700

    updates

--- home-manager/devs/rust/default.nix
+++ home-manager/devs/rust/default.nix
@@ -2,6 +2,7 @@
 {
   home.packages = with pkgs; [
     rustup
+    rustlings
     # openssl
     # pkg-config
   ];
--- home-manager/multiplexers/zellij/default.nix
+++ home-manager/multiplexers/zellij/default.nix
@@ -1,4 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  roomPlugin = pkgs.fetchurl {
+    url = "https://github.com/rvcas/room/releases/latest/download/room.wasm";
+    sha256 = "sha256-t6GPP7OOztf6XtBgzhLF+edUU294twnu0y5uufXwrkw=";
+  };
+in
 {
   programs.zellij = {
     enable = true;
@@ -6,4 +12,5 @@
   };
   xdg.configFile."zellij/config.kdl".source = ./config.kdl;
   xdg.configFile."zellij/layouts".source = ./layouts;
+  xdg.configFile."zellij/plugins/room.wasm".source = roomPlugin;
 }
