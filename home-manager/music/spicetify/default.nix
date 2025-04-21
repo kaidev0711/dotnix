a040acd9d6fe97025ce41f1f9adbfb520aacc026
Dang Van Tuan <kaiz.developers@gmail.com>
2025-04-22 00:03:16 +0700

    test

---
+++ home-manager/music/spicetify/default.nix
@@ -0,0 +1,25 @@
+{
+  inputs,
+  pkgs,
+  ...
+}:
+let
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
+in
+{
+  imports = [
+    inputs.spicetify-nix.homeManagerModules.default
+  ];
+
+  programs.spicetify = {
+    enable = true;
+    # theme = spicePkgs.themes.onepunch;
+    theme = spicePkgs.themes.catppuccin;
+    colorScheme = "mocha";
+
+    enabledExtensions = with spicePkgs.extensions; [
+      shuffle
+      adblock
+    ];
+  };
+}
