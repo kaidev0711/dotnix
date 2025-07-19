ff072b3df41e6c875ef4362c8bf348781193a481
Đặng Văn Tuấn <kaiz.developers@gmail.com>
2025-07-20 02:07:41 +0700

    up

--- home-manager/shells/nushell/default.nix
+++ home-manager/shells/nushell/default.nix
@@ -14,12 +14,11 @@
       $env.config.show_banner = false
       $env.config.history = {
         file_format: "sqlite"
-        max_size: 5_000_000
+        max_size: 1_000_000
         sync_on_enter: true
         isolation: true
       }
       $env.config.buffer_editor = "hx"
-      # $env.config.color_config = (default-dark)
       def --env y [...args] {
       	let tmp = (mktemp -t "yazi-cwd.XXXXXX")
       	yazi ...$args --cwd-file $tmp
