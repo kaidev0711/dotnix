f5eaef5ab68ed6829295b9978a16689a5836cfb8
Đặng Văn Tuấn <kaiz.developers@gmail.com>
2025-07-20 19:29:44 +0700

    nushell config

--- home-manager/shells/nushell/default.nix
+++ home-manager/shells/nushell/default.nix
@@ -12,12 +12,6 @@
     enable = true;
     extraConfig = ''
       $env.config.show_banner = false
-      $env.config.history = {
-        file_format: "sqlite"
-        max_size: 1_000_000
-        sync_on_enter: true
-        isolation: true
-      }
       $env.config.buffer_editor = "hx"
       def --env y [...args] {
       	let tmp = (mktemp -t "yazi-cwd.XXXXXX")
